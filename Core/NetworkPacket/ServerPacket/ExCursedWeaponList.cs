a5ab23d8100ab7c987c0f0154f2e339baa216262
Viacheslav <vyko@ciklum.com>
2021-08-13 11:15:20 +0300

    L2INT-157 Added RequestShowMiniMap Added RequestCursedWeaponList  (dummy) Added RequestCursedWeaponLocation (dummy)

---
+++ Core/NetworkPacket/ServerPacket/ExCursedWeaponList.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.NetworkPacket.ServerPacket
+{
+    internal sealed class ExCursedWeaponList : Network.ServerPacket
+    {
+        public override void Write()
+        {
+            WriteByte(0xfe);
+            WriteShort(0x45);
+            WriteInt(0);
+        }
+    }
+}
