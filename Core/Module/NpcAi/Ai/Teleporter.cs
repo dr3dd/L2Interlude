f85870ff9d7d45d779a52cb064ca0b25dad8e838
GL <glebsoluyanov@gmail.com>
2024-12-16 10:13:11 +0300

    *fix quest_pch2

--- Core/Module/NpcAi/Ai/Teleporter.cs
+++ Core/Module/NpcAi/Ai/Teleporter.cs
@@ -1,8 +1,7 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Core.Enums;
 using Helpers;
-using MySqlX.XDevAPI;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Core.Module.NpcAi.Ai;
 
